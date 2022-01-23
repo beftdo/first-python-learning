8ad344816254048a0391fdd101f4529ca8b9dd8c
beftdo <azurevane22@naver.com>
2022-01-23 20:23:56 +0900

    practice question

--- day2-2.py
+++ day2-2.py
@@ -33,3 +33,15 @@ print(randrange(38, 74))
 print(randrange(38, 74, 10))
 from random import random
 print(random())
+
+print("---------------------------------------------------------------------")
+#연습문제
+plain = {'a':1,'b':2, 'c':3}
+print(plain)
+fancy = OrderedDict([('a', 1),('b', 2),('c', 3),])
+print(fancy)
+
+from collections import defaultdict
+dict_of_lists = defaultdict(list)
+dict_of_lists['a'].append('something for a')
+print(dict_of_lists['a'])
