4aee6a12129c6da44284d049cab32236c277bd36
beftdo <azurevane22@naver.com>
2022-01-24 23:47:55 +0900

    Unicode+

--- day3.py
+++ day3.py
@@ -14,3 +14,25 @@ unicode_test('A')
 unicode_test('$')
 unicode_test('\u00a2')
 unicode_test('\u20ac')
+unicode_test('\u2603')
+print("---------------------------------------------------------------------")
+
+'''é를 유니코드 문자 이름 인덱스에서 찾으면 E WITH ACUTE, LATIN SMALL LETTER
+로 나오는데 파이썬에서 사용하는 유니코드 이름으로 바꾸려면 콤마 뒤에 오는 단어를 앞으로 
+옮기고 원래 콤마 앞에 있던 단어를 뒤로 옮겨야 한다. 바꾸면 LATIN SMALL LETTER E WITH ACUTE
+''' 
+import unicodedata
+print(unicodedata.lookup('LATIN SMALL LETTER E WITH ACUTE'))
+print(unicodedata.name('é'))
+
+u_umlant = '\N{LATIN SMALL LETTER U WITH DIAERESIS}'
+print(u_umlant)
+
+#문자열 len() 함수는 유니코드의 바이트가 아닌 문자수를 센다.
+print(len('$'))
+print(len('\u2603'))
+
+#유니코드의 숫자 id
+print(chr(233))
+print(chr(0xe9))
+print(chr(0x1fc6))
